76cf2bd557013b47db018d16dd6100732e455bf0
Seu Nome <seu-email@example.com>
2024-03-02 14:04:42 +0100

    mudança

---
+++ Untitled-3.js
@@ -0,0 +1,9 @@
+// Função para botão HTML
+function buttonClicked() {
+    alert("Botão HTML clicado!");
+}
+
+// Função para botão JavaScript
+function javascriptButtonClicked() {
+    alert("Botão JavaScript clicado!");
+}
